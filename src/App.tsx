b871d2a9d1b661f9fe64b76ddf2d1213ab9a1352
Breno Guilherme de Souza Lopes <brenogdev@gmail.com>
2023-01-18 21:46:27 -0300

    (feat): criado setup inicial do frontend

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles/global.css";
+
+export const App = () => {
+  return (
+    <div className="bg-slate-500 w-10 h-10 text-white rounded m-2 text-center">App</div>
+  );
+};
