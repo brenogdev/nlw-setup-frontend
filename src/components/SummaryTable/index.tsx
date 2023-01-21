12b937b06054d4a6b9f06ec6ac497fa226db7ce3
Breno Guilherme de Souza Lopes <brenogdev@gmail.com>
2023-01-21 13:04:47 -0300

    (feat): ajustes no layout da aplicação

--- src/components/SummaryTable/index.tsx
+++ src/components/SummaryTable/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { generateDatesFromYearBeginning } from "../../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "../HabitDay";
 import { WeekDays } from "../WeekDays";
+
+const summaryDates = generateDatesFromYearBeginning();
+const minimumSummaryDatesSize = 18 * 7; //18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export const SummaryTable: React.FC = () => {
   return (
@@ -8,21 +13,17 @@ export const SummaryTable: React.FC = () => {
       <WeekDays />
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+            <div
+              key={index}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          ))}
       </div>
     </div>
   );
